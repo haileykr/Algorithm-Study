b6edb0d7a2a191dac783b4ee804eeee810c1c799
haileykr <haileykmrp@gmail.com>
2021-11-29 00:04:45 +0900

    [PG, 87389] 나머지 1인 수
    https://programmers.co.kr/learn/courses/30/lessons/87389

--- programmers/PG_87389_나머지 1인 수.py
+++ programmers/PG_87389_나머지 1인 수.py
@@ -4,3 +4,15 @@
 https://programmers.co.kr/learn/courses/30/lessons/87389
 
 """
+
+
+def solution(n):
+    result = isPrimeNumber(n-1)
+    return n-1 if result == -1 else result
+
+
+def isPrimeNumber(n):
+    for i in range(2, n//2+1):
+        if n % i == 0:
+            return i
+    return -1
